5277ed034bb7d48a76f42400ddcb23ea5205bd42
Bahareh <Bahareh.Barakati@e-2.at>
2020-10-26 18:46:22 +0100

    Add update and delete functions * Add modal to html * Add updateEvent function in services * Add openModal function in calendar * Add some style

--- web-client/js/calendar.js
+++ web-client/js/calendar.js
@@ -1,6 +1,37 @@
-$(document).ready(function () {
-    getEvents();
+$('#exampleModal').modal({
+    show: 'false'
 });
+const calendarSetting = {
+    Color: '#999',                //(string - color) font color of whole calendar.
+    LinkColor: '#333',            //(string - color) font color of event titles.
+    NavShow: true,                //(bool) show navigation arrows.
+    NavVertical: false,           //(bool) show previous and coming months.
+    NavLocation: '#foo',          //(string - element) where to display navigation, if not in default position.
+    DateTimeShow: true,           //(bool) show current date.
+    DateTimeFormat: 'mmm, yyyy',  //(string - dateformat) format previously mentioned date is shown in.
+    DatetimeLocation: '',         //(string - element) where to display previously mentioned date, if not in default position.
+    EventClick: '',               //(function) a function that should instantiate on the click of any event. parameters passed in via data link attribute.
+    EventTargetWholeDay: false,   //(bool) clicking on the whole date will trigger event action, as opposed to just clicking on the title.
+    DisabledDays: [],             //(array of numbers) days of the week to be slightly transparent. ie: [1,6] to fade Sunday and Saturday.
+};
+
+function openModal(eventId) {
+    $('#exampleModal').modal('show');
+    $('#delete-button').on('click', function () {
+        $('#exampleModal').modal('hide');
+        deleteEvent(eventId);
+    });
+    $('#update-button').on('click', function () {
+        
+        if ($('#update-date').val() && $('#update-time').val()) {
+            let newDate = $('#update-date').val() + ' ' + $('#update-time').val();
+            $('#exampleModal').modal('hide');
+            updateEvent(eventId, newDate);
+        } else {
+            alert('Please Provide Date time');
+        }
+    });
+}
 
 function loadCalendar(events) {
     let eventsCalendar = [];
@@ -9,11 +40,15 @@ function loadCalendar(events) {
         let year = new Date(value.date).getFullYear();
         let day = new Date(value.date).getDate();
         let month = new Date(value.date).getMonth();
+        let eventColor = '#' + value.hex_color;
+        let title = '<div class="event" style="background-color:' + eventColor + '">';
+        title += time + ' ' + value.category_name + ' , ' + value.home_team + ' - ' + value.away_team;
+        title += '</div>';
         eventsCalendar.push({
             Date: new Date(year, month, day),
-            Title: time + ' ' + value.category_name + ' , ' + value.home_team + ' - ' + value.away_team,
+            Title: title,
             Link: function () {
-                deleteEvent(value.id)
+                openModal(value.id);
             }
         })
     });
@@ -21,3 +56,7 @@ function loadCalendar(events) {
     $('#events-list').empty();
     caleandar(document.querySelector('#events-list'), eventsCalendar, calendarSetting);
 }
+
+$(document).ready(function () {
+    getEvents();
+});
--- web-client/js/services.js
+++ web-client/js/services.js
@@ -15,14 +15,27 @@ function addEvent(e) {
         alert('Unable to add event');
     });
 };
-
-function deleteEvent(id) {
+function updateEvent(eventId, eventDate) {
+    $.ajax({
+        url: 'http://localhost:8000/events/update',
+        method: 'POST',
+        data: {
+            event_id: eventId,
+            new_date: eventDate,
+        }
+    }).done(function () {
+        getEvents();
+    }).fail(function () {
+        alert('Unable to Update the event');
+    });
+}
+function deleteEvent(eventId) {
     if (confirm('Are you sure you want to delete this event?')) {
         $.ajax({
             url: 'http://localhost:8000/events/delete',
             method: 'POST',
             data: {
-                event_id: id
+                event_id: eventId
             }
         }).done(function () {
             getEvents();
@@ -31,20 +44,6 @@ function deleteEvent(id) {
         });
     }
 }
-
-const calendarSetting = {
-    Color: '#999',                //(string - color) font color of whole calendar.
-    LinkColor: '#333',            //(string - color) font color of event titles.
-    NavShow: true,                //(bool) show navigation arrows.
-    NavVertical: false,           //(bool) show previous and coming months.
-    NavLocation: '#foo',          //(string - element) where to display navigation, if not in default position.
-    DateTimeShow: true,           //(bool) show current date.
-    DateTimeFormat: 'mmm, yyyy',  //(string - dateformat) format previously mentioned date is shown in.
-    DatetimeLocation: '',         //(string - element) where to display previously mentioned date, if not in default position.
-    EventClick: '',               //(function) a function that should instantiate on the click of any event. parameters passed in via data link attribute.
-    EventTargetWholeDay: false,   //(bool) clicking on the whole date will trigger event action, as opposed to just clicking on the title.
-    DisabledDays: [],             //(array of numbers) days of the week to be slightly transparent. ie: [1,6] to fade Sunday and Saturday.
-};
 
 function getEvents() {
     $.ajax({
@@ -57,7 +56,6 @@ function getEvents() {
         alert('Unable to load events');
     });
 };
-
 
 function filterEvents(e) {
     e.preventDefault();
